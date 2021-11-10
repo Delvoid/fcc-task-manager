ccc6fbabd4a889861f0f3572f4b6a6f4017e2fd3
Delvoid <delvoidltd@gmail.com>
2021-11-10 19:09:20 +0000

    Database setup and Task model created

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const connectDb = require('./db/connectDb')
 const tasks = require('./routes/tasks')
-
 require('dotenv').config()
+
 const app = express()
+
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
@@ -13,10 +15,14 @@ app.get('/hello', (req, res) => {
 })
 
 app.use('/api/v1/tasks', tasks)
-//app.get('/api/v1/tasks')          -get all tasks
-//app.post('/api/v1/tasks')         - create a new task
-//app.get('/api/v1/tasks/:id')      - get stingle task
-//app.patch('/api/v1/tasks/:id')    - update task
-//app.delete('/api/v1/tasks/:id')   - delete task
 
-app.listen(PORT, console.log(`server is listening on port ${PORT}`))
+const start = async () => {
+  try {
+    await connectDb()
+    app.listen(PORT, console.log(`server is listening on port ${PORT}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
--- controller/tasks.js
+++ controller/tasks.js
@@ -1,7 +1,25 @@
 const getAllTasks = (req, res) => {
-  res.send('all items from file')
+  res.json('get all tasks')
+}
+
+const createTask = (req, res) => {
+  res.json(req.body)
+}
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id })
+}
+const updateTask = (req, res) => {
+  res.json({ id: req.params.id })
+}
+const deleteTask = (req, res) => {
+  res.json({ id: req.params.id })
 }
 
 module.exports = {
   getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 }
