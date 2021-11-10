cacdb8639ba003ff694d304690c6e2b28e36d995
Delvoid <delvoidltd@gmail.com>
2021-11-10 20:26:49 +0000

    task update endpoint

--- controller/tasks.js
+++ controller/tasks.js
@@ -32,9 +32,20 @@ const getTask = async (req, res) => {
     res.status(500).json({ msg: error })
   }
 }
-const updateTask = (req, res) => {
-  res.json({ id: req.params.id })
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params
+    const task = await Task.findByIdAndUpdate(taskId, req.body, { new: true, runValidators: true })
+    if (!task) return res.status(404).json({ msg: `no task with id : ${taskId}` })
+
+    res.status(200).json({ task })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ msg: error })
+  }
 }
+
 const deleteTask = async (req, res) => {
   try {
     const { id: taskId } = req.params
